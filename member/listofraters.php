370fd5811941951e700fdaa843737efb24342514
blandline <sarbuland1000@gmail.com>
2023-06-07 14:05:21 +0800

    testing form processing

--- member/listofraters.php
+++ member/listofraters.php
@@ -12,17 +12,27 @@
 </head>
    
 <body>
-    <?php include 'rater-list.php'?>
+
 <header>
 
 </header>
 <main>
     <div id="allcontainer">
     <div id="topbar">
+    <?php
+if (isset($_GET['submit']))
+{
+        $firstname = $_GET['focus_first_name'];
+       
+       
+}
+?>
 
 </div>
 <div id="formcontainer">
-<form method="post" id="rateform" action="rater-list.php">
+<form method="get" id="rateform" action="listofraters.php">
+
+
     <table id="raterlisttable">
         <tr>
             <th colspan="2"><p>FOCUS Name</p></th>
@@ -51,30 +61,55 @@
             <td><input type="text" name="Rater-last-name"></td>
             <td>
                 <select name="Roles" id="roles">
-                    <option value="Focus" name>FOCUS</option>
-                    <option value="manager" name>Manager</option>
-                    <option value="colleague" name>Colleague</option>
-                    <option value="direct-report" name>Direct report</option>
-                    <option value="other" name>Other</option>
+                    <option value="FOCUS" name='focus_role'>FOCUS</option>
+                    <option value="Manager" name='manager_role'>Manager</option>
+                    <option value="Colleague" name='colleague_role'>Colleague</option>
+                    <option value="Direct report" name='direct_report_role'>Direct report</option>
+                    <option value="Other" name='other_role'>Other</option>
                 </select>
             </td>
             <td>
                 <select name="Genders" id="genders">
-                    <option value="Focus" name>Male</option>
-                    <option value="manager" name>Female</option>
-                    <option value="other-gender" name>Other Gender</option>
+                    <option value="Male" name='male_gender'>Male</option>
+                    <option value="Female" name='female_gender'>Female</option>
+                    <option value="Other Gender" name='other_gender'>Other Gender</option>
                     
                 </select>
             </td>
             <td><input type="text" name="position"></td>
             <td><input type="text" name="email"></td>
         </tr>
-    </table>
-    <input type="submit" style="background-color:rgb(210, 56, 56); border-color:rgb(253, 253, 255); color:rgb(0, 0, 0)">Activate
+    </table> 
+    <input type="submit" style="background-color:rgb(210, 56, 56); border-color:rgb(253, 253, 255); color:rgb(0, 0, 0)" value="Activate">
 </form>
+
 </div>
     </div>
-    
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // extract the form data using the $_GET superglobal variable
+    $FOCUS_first_name = $_GET['FOCUS_first_name'];
+    $FOCUS_last_name = $_GET['FOCUS_last_name'];
+    $Launch_date = $_GET['Launch-date'];
+    $End_date = $_GET['End-date'];
+    $Rater_first_name = $_GET['Rater-first-name'];
+    $Rater_last_name = $_GET['Rater-last-name'];
+    $Roles = $_GET['Roles'];
+    $Genders = $_GET['Genders'];
+    $position = $_GET['position'];
+    $email = $_GET['email'];
+
+    // output the form data
+    echo "<p>FOCUS Name: {$FOCUS_first_name} {$FOCUS_last_name}</p>";
+    echo "<p>Launch Date: {$Launch_date}</p>";
+    echo "<p>End Date: {$End_date}</p>";
+    echo "<p>Rater Name: {$Rater_first_name} {$Rater_last_name}</p>";
+    echo "<p>Role: {$Roles}</p>";
+    echo "<p>Gender: {$Genders}</p>";
+    echo "<p>Position: {$position}</p>";
+    echo "<p>Email: {$email}</p>";
+    }
+?>
     
 
     <button onclick="addrow()">Add</button>
--- member/rater-list.php
+++ member/rater-list.php
@@ -1,51 +1,25 @@
-
 <?php
-// Define database connection variables
-// $servername = "localhost";
-// $username = "username";
-// $password = "password";
-// $dbname = "database_name";
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // extract the form data using the $_GET superglobal variable
+    $FOCUS_first_name = $_GET['FOCUS_first_name'];
+    $FOCUS_last_name = $_GET['FOCUS_last_name'];
+    $Launch_date = $_GET['Launch-date'];
+    $End_date = $_GET['End-date'];
+    $Rater_first_name = $_GET['Rater-first-name'];
+    $Rater_last_name = $_GET['Rater-last-name'];
+    $Roles = $_GET['Roles'];
+    $Genders = $_GET['Genders'];
+    $position = $_GET['position'];
+    $email = $_GET['email'];
 
-// // Create database connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Check if the connection is successful
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-
-// Get form data
-$focus_first_name = $_POST['FOCUS_first_name'];
-$focus_last_name = $_POST['FOCUS_last_name'];
-// $start_date = $_POST['Launch-date'];
-// $end_date = $_POST['End-date'];
-// $roles = $_POST['roles'];
-// $genders = $_POST['genders'];
-// $position = $_POST['position'];
-// $employee_id = $_POST['employee_id'];
-// $employee_first_name = $_POST['Rater_first_name'];
-// $employee_last_name = $_POST['Rater_last_name'];
-
-// Prepare SQL statement to insert data into FOCUS table
-//$sql_FOCUS = "INSERT INTO FOCUS (FOCUS_first_name, FOCUS_last_name, start_date, end_date, roles, genders, position, employee_id) VALUES ('$FOCUS_first_name', '$FOCUS_last_name', '$start_date', '$end_date', '$roles', '$genders', '$position', '$employee_id')";
-
-// Execute SQL statement to insert data into FOCUS table
-// if ($conn->query($sql_FOCUS) === TRUE) {
-//   echo "Data added to FOCUS table successfully.";
-// } else {
-//   echo "Error: " . $sql_FOCUS . "<br>" . $conn->error;
-// }
-echo $focus_first_name;
-// Prepare SQL statement to insert data into employee table
-// $sql_employee = "INSERT INTO employee (employee_first_name, employee_last_name, employee_id, roles, genders, position) VALUES ('$employee_first_name', '$employee_last_name', '$employee_id', '$roles', '$genders', '$position')";
-
-// Execute SQL statement to insert data into employee table
-// if ($conn->query($sql_employee) === TRUE) {
-//   echo "Data added to employee table successfully.";
-// } else {
-//   echo "Error: " . $sql_employee . "<br>" . $conn->error;
-// }
-
-// Close database connection
-// $conn->close();
+    // output the form data
+    echo "<p>FOCUS Name: {$FOCUS_first_name} {$FOCUS_last_name}</p>";
+    echo "<p>Launch Date: {$Launch_date}</p>";
+    echo "<p>End Date: {$End_date}</p>";
+    echo "<p>Rater Name: {$Rater_first_name} {$Rater_last_name}</p>";
+    echo "<p>Role: {$Roles}</p>";
+    echo "<p>Gender: {$Genders}</p>";
+    echo "<p>Position: {$position}</p>";
+    echo "<p>Email: {$email}</p>";
+    }
 ?>
